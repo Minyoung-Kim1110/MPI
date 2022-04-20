4993f0390808186ce478bbe182fc3e6aec4a756d
kyunghoon-jung <khoony.jung@gmail.com>
2022-04-20 11:30:41 +0900

    correct compile command

--- numerical_integration_ver2.c
+++ numerical_integration_ver2.c
@@ -3,7 +3,7 @@ Minyoung Kim (Apr. 19, 2022)
 2022 Spring PSC class 
 Subject: MPI
 MPI softward: msmpi // msmpi use gcc to compile mpi code 
-compile:mpicc -g ${file} -o ${fileDirname}/${fileBasenameNoExtension}.exe
+compile:mpicc -g ${file} -o ${fileDirname}/${fileBasenameNoExtension}.exe -lm
 */
 
 #include <stdlib.h>
